2b9f9a2aac872ac7aa2edb05fbe4cdc839d3dff7
CJ <charlottejowen93@gmail.com>
2020-02-15 09:53:45 +0000

    made a start

--- app_data_prep.R
+++ app_data_prep.R
@@ -2,49 +2,73 @@
 library(tidyverse)
 library(parlitools)
 library(readxl)
-polls <- read_excel("Scheduled Polls 2017-2025 (1).xlsx")
+library(pdftools)
+library(tabulizer)
+library(skimr)
 
-ons_codes_LAs <- la_codes
-ons_codes_const <- bes_2015 %>% select(ons_const_id,constituency_name,country,region,constituency_type)
-colnames(ons_codes_LAs)
-ons_codes_LAs %>% filter(type!="Scotland"&type!="Wales"&type!="Northern Ireland") #20 too many for England Apr 2020?
-colnames(ons_codes_const)
-ons_codes_const <- ons_codes_const %>% rename(name=constituency_name, code=ons_const_id, type=constituency_type)
-colnames(ons_codes_const)
-ons_codes_LAs <- ons_codes_LAs %>% rename(code=la_code)
-colnames(ons_codes_const)
-colnames(ons_codes_LAs)
+"The English election timetable can be accessed here: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/792138/Election_Timetable_in_England_2019.pdf"
+"The parlitools package contains a list of all local government authorities in the UK and their unique codes. Most helpfully 
+here, it contains what type of local area they are too."
 
-temp <- polls$`Authority Name`
-temp <- gsub("Ards and North Down", "North Down and Ards", temp)
-temp <- gsub("Armagh, Banbridge and Craigavon", "Armagh City, Banbridge and Craigavon", temp)
-temp <- gsub("Bristol, City of", "City of Bristol", temp)
-temp <- gsub("Edinburgh", "City of Edinburgh", temp)
-temp <- gsub("Glasgow", "Glasgow City", temp) 
-temp <- gsub("Herefordshire, County of", "Herefordshire", temp)
-temp <- gsub("Kingston upon Hull, City of", "Kingston upon Hull", temp)
-temp <- gsub("Kingston Upon Thames", "Kingston upon Thames", temp)
-temp <- gsub("Newcastle Upon Tyne", "Newcastle upon Tyne", temp)
-temp <- gsub("Orkney", "Orkney Islands", temp)
-temp <- gsub("Rhondda Cynon Taf", "Rhondda Cynon Taff", temp)
-temp <- gsub("Richmond Upon Thames", "Richmond upon Thames", temp)
-temp <- gsub("Rotheram", "Rotherham", temp)
-temp <- gsub("Shepway", "Folkestone and Hythe", temp)
-temp <- gsub("Shetland", "Shetland Islands", temp)
-temp <- gsub("St. Albans", "St Albans", temp)
-temp <- gsub("St. Edmundsbury", "St Edmundsbury", temp)
-temp <- gsub("St. Helens", "St Helens", temp)
-polls$`Authority Name` <- temp	
+df <- la_codes %>% filter(type!="Sui Generis"&type!="Scotland"&type!="Wales"&type!="Northern Ireland")
 
-ons_codes_LAs <- ons_codes_LAs %>% 
-	mutate(country = ifelse(str_detect(ons_codes_LAs$code,"W")==TRUE, "Wales", ifelse(str_detect(ons_codes_LAs$code,"E")==TRUE, "England", ifelse(str_detect(ons_codes_LAs$code,"S")==TRUE, "Scotland","Northern Ireland"))))
+skim(df)
 
-colnames(ons_codes_const)
+"There are 3 districts different to the rest, Doncaster, Rotherham and Birmingham"
+temp <- c("Doncaster","Rotherham","Birmingham")
+met_whole <- df %>% filter(name %in% temp) #every year except council year
 
-test <- left_join(polls,ons_codes_LAs, by = c("Authority Name"="name"))
+`%notin%` <- Negate(`%in%`)
+met_thirds <- df %>% filter(type=="Metropolitan"&name %notin% temp) #once every 4 years. from 16/17/18 for Rotherham, Doncaster, Birmingham
 
-view(test[is.na(test$code)==TRUE,])	
-class(polls$Year)
+rm(temp)
 
-write_csv(polls,"Scheduled_polls.csv")
+london <- df %>% filter(type=="London") #every 4 years from 2022
+county <- df %>% filter(type=='County') #online list doesn't include Dorset, has it as a UA instead
+#every 4 years from 2021
 
+district_whole_A <- c("") #4 years from 2023
+district_whole_B <- c("") #4 years from 2020
+district_whole_C <- c("") #4 years from 2022
+district_thirds <- c("") #every year except council year 
+district_halves <- c("") #every 2 years from 2020
+
+
+#met districts are mainly elected by thirds, those that are not:
+# Doncaster, Rotherham, Birmingham all elect whole council
+# Districts are weird and will need lot of separate info
+# Unitary Authorities
+# 
+
+### Correct modulo input shown below, fix - then nearly ready!
+which_elections <- function(year){
+  elec.c = ifelse((year-2021) %% 4 == 0, "TRUE","FALSE") #county
+  elec.b = ifelse((year-2022) %% 4 == 0, "TRUE","FALSE") #london boroughs
+  elec.doncaster = ifelse((year-2021) %% 4 == 0, "TRUE","FALSE") 
+  elec.rotherham = ifelse((year-2020) %% 4 == 0, "TRUE","FALSE")
+  elec.birmingham = ifelse((year-2022) %% 4 == 0, "TRUE","FALSE")
+  elec.dwA = ifelse((year-2023) %% 4 == 0, "TRUE","FALSE") #district whole, group A
+  elec.dwB = ifelse((year-2020) %% 4 == 0, "TRUE","FALSE") #district whole, group B
+  elec.dwC = ifelse((year-2022) %% 4 == 0, "TRUE","FALSE") #district whole, group C
+  elec.d2 = ifelse(year %% 2 == 0,"FALSE","TRUE") #district by halves
+  elec.d3 = ifelse(elec.c == "TRUE","FALSE","TRUE") #district by thirds
+  elec.met3 = ifelse(elec.c == "TRUE","FALSE","TRUE") #metropolitan thirds
+  elec.u3 = ifelse(elec.c == "TRUE","FALSE","TRUE") #unitary thirds
+  elec.uwA = ifelse((year-2023) %% 4 == 0, "TRUE","FALSE") #unitary whole, group A
+  elec.uwB = ifelse((year-2021) %% 4 == 0, "TRUE","FALSE") #unitary whole, group A
+  elec.uwC = ifelse((year-2020) %% 4 == 0, "TRUE","FALSE") #unitary whole, group A
+  elec.sandc = ifelse((year-2021) %% 4 == 0, "TRUE","FALSE") #scilly and city
+  elecs <- list(elec.c,elec.b,elec.doncaster)
+  print("The areas that have elections this year are:")
+  for(e in elecs){
+    if(e == TRUE){
+      print(deparse(substitute(e)))
+    }
+  }
+}
+
+which_elections(2034)
+
+test_year <- 2025
+test_out <- ifelse((test_year-2021)%%4 == 0, "TRUE","FALSE")
+test_out
